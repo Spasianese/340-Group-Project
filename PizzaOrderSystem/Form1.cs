72c0ad1e4b489db4fc3a231e352f68cddb885d86
pizza <pizza@LAPTOP-S8NOK7HL>
2023-11-16 01:37:11 -0500

    Add project files.

---
+++ PizzaOrderSystem/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaOrderSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel2.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel2.Show();
+            label1.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel2.Hide();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /*
+            string message = "The email has been used already.  Please pick another email.";
+            string title = "Error";
+            MessageBox.Show(message, title);
+            
+            string message = "Congratulation! You are our new member #1001.";
+            string title = "Welcome";
+            MessageBox.Show(message, title);
+            */
+
+        }
+    }
+}
